bf0a1f9d391ea0af319f507b6ab4b90a4cc73a72
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-05 22:37:11 +0900

    feat: Create style components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@ import axios from "../api/axios";
 import React, { useState, useEffect } from "react";
 import requests from "../api/requests";
 import "./Banner.css";
+import styled from "styled-components";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -19,7 +21,6 @@ export default function Banner() {
 
     // 특정 영화의 더 상세한 정보 가져오기(비디오 정보도 포함)
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, { params: { append_to_response: "videos" } });
-    console.log(movieDetail, movieDetail.backdrop_path);
 
     setMovie(movieDetail);
   };
@@ -28,29 +29,80 @@ export default function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        {/**Tittle */}
-        <hi className="banner__title">{movie.title || movie.name || movie.original_name}</hi>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">
-            <div className="space"></div>More Information
-          </button>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          {/**Tittle */}
+          <hi className="banner__title">{movie.title || movie.name || movie.original_name}</hi>
+          <div className="banner__buttons">
+            <button className="banner__button play" onClick={() => setIsClicked(true)}>
+              Play
+            </button>
+            <button className="banner__button info">
+              <div className="space"></div>More Information
+            </button>
+          </div>
+          {/**Div > 2 BUTTONS */}
+          <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+          {/*Description*/}
         </div>
-        {/**Div > 2 BUTTONS */}
-        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-        {/*Description*/}
-      </div>
-      <div className="banner--fadeBottom"></div>
-    </header>
-  );
+        <div className="banner--fadeBottom"></div>
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
