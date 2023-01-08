45182b863c9df7ea242bf8bbf8d5e7702dca3430
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-08 19:21:14 +0900

    feat: Create search input bar
    - handle keywords changing in input bar
    - navigate by search keywords directly

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,6 +21,11 @@ export default function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -26,6 +34,9 @@ export default function Nav() {
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
+
+      <input value={searchValue} onChange={handleChange} className="nav__input" type="text" />
+
       <img alt="User logged" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" className="nav__avatar" />
     </nav>
   );
