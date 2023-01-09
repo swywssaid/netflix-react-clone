b5c81828175a88b488bdcf425786b3f226ab81f9
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-09 16:33:18 +0900

    feat: Fetch the searched movies

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,5 +1,29 @@
-import React from "react";
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function SearchPage() {
-  return <div>index</div>;
+  const [searchResult, setSearchResult] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) fetchSearchMovie(searchTerm);
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResult(request.data.results);
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  return <div></div>;
 }
