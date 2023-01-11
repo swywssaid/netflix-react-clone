044882865df8b991cad03c3ad61d9c1144fa12a0
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-11 20:50:15 +0900

    feat: Create touch slide feature
    - using swiper library

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,17 @@ import React, { useState, useEffect } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal";
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export default function Row({ title, fetchUrl, isLargeRow, id }) {
   // 영화 정보 가져오기
   const [movies, setMovies] = useState([]);
@@ -29,39 +40,49 @@ export default function Row({ title, fetchUrl, isLargeRow, id }) {
     <section className="row">
       {/**Title */}
       <h2>{title}</h2>
-      <div className="slider">
-        <div
-          className="slider__arrow-left"
-          onClick={() => {
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{"<"}</span>
-        </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // loop 기능을 사용할 것인지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 6, // 몇개씩 슬라이드 할지
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} // 페이지 버튼 보이게 할지
+      >
         <div id={id} className="row__posters">
           {/**SEVERAL ROW__POSTER */}
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={() => {
-                openModal(movie);
-              }}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                loading="lazy"
+                alt={movie.name}
+                onClick={() => {
+                  openModal(movie);
+                }}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div
-          className="slider__arrow-right"
-          onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{">"}</span>
-        </div>
-      </div>
+      </Swiper>
       {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
     </section>
   );
