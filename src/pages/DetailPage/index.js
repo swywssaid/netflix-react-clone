8450ef605cf71afcb0791c0d14d9d932cee8f720
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-11 19:59:39 +0900

    feat: Create DetailPage

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,5 +1,24 @@
-import React from "react";
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function DetailPage() {
-  return <div>DetailPage</div>;
+  let { movieId } = useParams();
+  const [movies, setMovies] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`);
+      setMovies(request.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movies) return null;
+
+  return (
+    <section>
+      <img className="modal__poster-img" src={`https://image.tmdb.org/t/p/original/${movies.backdrop_path}`} alt="modal__poster-img" />
+    </section>
+  );
 }
